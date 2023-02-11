8826aa95ced6dfd6843b04b749f21e55a4aa14af
Robin Steeman <robin@tracefy.com>
2023-02-11 07:36:41 +0100

    initial expo setup with react-three-fiber

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { useRef, useState } from "react"
+import { Canvas, useFrame } from "@react-three/fiber"
+import { View, StyleSheet } from "react-native"
+
+function Box(props: any) {
+    const mesh = useRef(null)
+    // const [hovered, setHover] = useState(false)
+    // const [active, setActive] = useState(false)
+    // useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
+    return (
+        <mesh
+            {...props}
+            ref={mesh}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={'orange'} />
+        </mesh>
+    )
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Canvas>
+                <ambientLight />
+                <pointLight position={[10, 10, 10]} />
+                <Box position={[0, 0, 0]} />
+            </Canvas>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "black",
+    },
 });
